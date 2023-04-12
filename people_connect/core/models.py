b146465fef5671f6d420302ece8c04c61d081962
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2023-04-12 22:42:25 +0530

    ready to go with colabs

--- people_connect/core/models.py
+++ people_connect/core/models.py
@@ -28,7 +28,7 @@ class FollowersCount(models.Model):
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='post_images')
+    image = models.ImageField(upload_to='post_images/')
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
